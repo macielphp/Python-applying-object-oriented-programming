16cbbb974dc4d05bb7fedd90fc32639ab1843ff2
Maciel Santos <132824821+macielphp@users.noreply.github.com>
2024-09-05 17:52:27 -0300

    Done constructor

--- models/restaurants.py
+++ models/restaurants.py
@@ -1,12 +1,17 @@
 class Restaurant: 
-    name = ''
-    category = ''
-    active = False
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        self.active = False
+        
 
-restaurant_park = Restaurant()
-restaurant_park.name = 'Park'
-restaurant_park.category = 'Gourmet'
-restaurant_pizza = Restaurant()
+restaurant_park = Restaurant('Park', 'Gormet')
+restaurant_pizza = Restaurant('Pizza Mix', 'Brazilian')
+restaurant_dutto = Restaurant('Dutto', 'Italian')
 
-restaurants = [restaurant_park, restaurant_pizza]
-print(restaurant_park.name)
+
+restaurants = [restaurant_park, restaurant_pizza, restaurant_dutto]
+
+print(vars(restaurant_park))
+print(vars(restaurant_dutto))
+print(vars(restaurant_pizza))
