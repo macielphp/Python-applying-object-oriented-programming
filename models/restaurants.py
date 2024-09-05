54928a86aa14b964195e9cc7329fe670c141ee13
Maciel Santos <132824821+macielphp@users.noreply.github.com>
2024-09-05 19:53:02 -0300

    Transform Restaurant's class attribuites to private

--- models/restaurants.py
+++ models/restaurants.py
@@ -4,15 +4,19 @@ class Restaurant:
     def __init__(self, name, category):
         self.name = name
         self.category = category
-        self.active = False
+        self._active = False #_active is a private attribuite.
         Restaurant.restaurants.append(self)
 
     def __str__(self):
         return f'{self.name} | {self.category}'
     
-    def list_restaurants():
+    def list_restaurants(): 
+        print(f'{"Restaurant's name".ljust(25)} | {"Category".ljust(25)} | {"Status"}')
         for restaurant in Restaurant.restaurants:
-            print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}') 
+            print(f'{restaurant.name.ljust(25)} | {restaurant.category.ljust(25)} | {restaurant.active}') 
+    @property
+    def active(self):
+        return '✅' if self._active else '❌'
 
 restaurant_park = Restaurant('Park', 'Gormet')
 restaurant_pizza = Restaurant('Pizza Mix', 'Brazilian')
