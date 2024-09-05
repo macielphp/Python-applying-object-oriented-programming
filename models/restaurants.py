558b818e2ca47fc69a634494367826365c540956
Maciel Santos <132824821+macielphp@users.noreply.github.com>
2024-09-05 18:13:38 -0300

    Done Restaurant.restaurants list, list_restaurants and __str__(self)

--- models/restaurants.py
+++ models/restaurants.py
@@ -1,17 +1,22 @@
 class Restaurant: 
+    restaurants = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
         self.active = False
-        
+        Restaurant.restaurants.append(self)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
+    
+    def list_restaurants():
+        for restaurant in Restaurant.restaurants:
+            print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}') 
 
 restaurant_park = Restaurant('Park', 'Gormet')
 restaurant_pizza = Restaurant('Pizza Mix', 'Brazilian')
 restaurant_dutto = Restaurant('Dutto', 'Italian')
 
-
-restaurants = [restaurant_park, restaurant_pizza, restaurant_dutto]
-
-print(vars(restaurant_park))
-print(vars(restaurant_dutto))
-print(vars(restaurant_pizza))
+Restaurant.list_restaurants()
+ 
